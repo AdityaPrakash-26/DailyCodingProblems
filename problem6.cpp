e904c70015fea8905354393488829f5ef6e1be02
Aditya Prakash <prakashadityaap@gmail.com>
2021-06-17 19:03:21 +0530

    p6

--- problem6.cpp
+++ problem6.cpp
@@ -6,7 +6,8 @@ class Node {
 };
 
 Node* Xor(Node* x, Node* y){
-    return reinterpret_cast<Node*>(
-        reinterpret_cast<uintptr_t>(x)
-        ^ reinterpret_cast<uintptr_t>(y));
+    return reinterpret_cast<Node*>(reinterpret_cast<uintptr_t>(x) ^ reinterpret_cast<uintptr_t>(y));
 }
+
+
+
