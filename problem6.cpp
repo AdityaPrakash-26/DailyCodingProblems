97ab66d463b44128df3ebc0bfdb3cfcb8e8419ab
Aditya Prakash <prakashadityaap@gmail.com>
2021-06-17 18:48:53 +0530

    p6

---
+++ problem6.cpp
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+class Node {
+    int val = 0;
+    Node* xnode;
+};
+
+Node* Xor(Node* x, Node* y){
+    return reinterpret_cast<Node*>(
+        reinterpret_cast<uintptr_t>(x)
+        ^ reinterpret_cast<uintptr_t>(y));
+}
