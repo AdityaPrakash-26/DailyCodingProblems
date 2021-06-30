451782f2fd71ac817b3da1d516254dafde1e30fc
Aditya Prakash <prakashadityaap@gmail.com>
2021-06-30 09:42:56 +0530

    problem7.cpp

--- Problems 001-010/problem7.cpp
+++ Problems 001-010/problem7.cpp
@@ -1,3 +1,9 @@
+/* This question is asked by Facebook. Given a string and the ability to delete at 
+ * most one character, return whether or not it can form a palindrome.
+ * Note: a palindrome is a sequence of characters that reads the same forwards and 
+ * backwards.
+ */
+
 #include <iostream>
 #include <string>
 using namespace std;
