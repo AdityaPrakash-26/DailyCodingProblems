87f3d40d549173e1f74eef7f0fe0bf3cebae1210
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-13 10:48:02 +0530

    problem10.cpp

--- Problems 010-099/problem10.cpp
+++ Problems 010-099/problem10.cpp
@@ -1,3 +1,10 @@
+/*
+ * This question is asked by Facebook. Given two strings s and t return 
+ * whether or not s is an anagram of t.
+ * Note: An anagram is a word formed by reordering the letters of another 
+ * word.
+*/
+
 #include <iostream>
 #include <string>
 #include <unordered_map>
@@ -7,20 +14,31 @@ int main() {
     string s,t;
     cin>>s>>t;
 
-    unordered_map<char, int> sMap;
-    unordered_map<char, int> tMap;
+    int freqArrayForTheSString[26] = {0};
+    int freqArrayForTheTString[26] = {0};
 
     for(int i = 0; i<s.length(); i++){
-        sMap[s[i]]++;
+        freqArrayForTheSString[s[i] - 'a']++;
     }
 
     for(int i = 0; i<t.length(); i++){
-        tMap[s[i]]++;
+        freqArrayForTheTString[t[i] - 'a']++;
     }
 
     bool hasTheSameHashMap = true;
 
-    for(int i = 0; i<tMap.size(); i++){
-        
+    for(int i = 0; i<26; i++){
+        if(freqArrayForTheSString[i] == freqArrayForTheTString[i]){
+            //do nothing
+        } else {
+            hasTheSameHashMap = false;
+            break;
+        }
+    }
+
+    if(hasTheSameHashMap){
+        cout<<"YES\n";
+    } else {
+        cout<<"NO\n";
     }
 }
