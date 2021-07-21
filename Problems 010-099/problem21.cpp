9c085260e4ba0f21f7a32d62cbd9e3f69a60cac9
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-21 10:12:47 +0530

    problem21

---
+++ Problems 010-099/problem21.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+//implementation for node of a linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+
+Node* reverseLinkedList(Node* head){
+
+    Node* p1 = NULL;
+    Node* p2 = head;
+    Node* p3 = NULL;
+
+    while(p2 != NULL){
+        p3 = p2->next;
+        p2->next = p1;
+        p1 = p2;
+        p2 = p3;
+    }
+    return p1;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    Node* reversed = reverseLinkedList(head);
+
+    while(reversed != NULL){
+        cout << reversed->data << " ";
+        reversed = reversed->next;
+    }
+    return 0;
+}
